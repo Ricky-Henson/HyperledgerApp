6aaaf10af35b827cf4d4a4a355f3216c23fe62f1
Tony <khunkhin444@gmail.com>
2023-11-13 14:49:37 +0800

    Fix view problem (hashkey now not viewable in view all employee)

--- employee-asset-transfer/chaincode/lib/primary-contract.js
+++ employee-asset-transfer/chaincode/lib/primary-contract.js
@@ -69,24 +69,31 @@ class PrimaryContract extends Contract {
     let allResults = [];
     while (true) {
       let res = await iterator.next();
-
+  
+      // Check if res.value exists and its value can be converted to string
       if (res.value && res.value.value.toString()) {
-        let jsonRes = {};
-        console.log(res.value.value.toString("utf8"));
-
-        if (isHistory && isHistory === true) {
-          jsonRes.Timestamp = res.value.timestamp;
+        // Check if the key starts with 'EID'
+        const isEID = res.value.key.startsWith('EID');
+        
+        if (isEID) {
+          let jsonRes = {};
+          console.log(res.value.value.toString("utf8"));
+  
+          if (isHistory && isHistory === true) {
+            jsonRes.Timestamp = res.value.timestamp;
+          }
+          jsonRes.Key = res.value.key;
+  
+          try {
+            jsonRes.Record = JSON.parse(res.value.value.toString("utf8"));
+          } catch (err) {
+            console.log(err);
+            jsonRes.Record = res.value.value.toString("utf8");
+          }
+          allResults.push(jsonRes);
         }
-        jsonRes.Key = res.value.key;
-
-        try {
-          jsonRes.Record = JSON.parse(res.value.value.toString("utf8"));
-        } catch (err) {
-          console.log(err);
-          jsonRes.Record = res.value.value.toString("utf8");
-        }
-        allResults.push(jsonRes);
       }
+  
       if (res.done) {
         console.log("end of data");
         await iterator.close();
@@ -95,5 +102,36 @@ class PrimaryContract extends Contract {
       }
     }
   }
+  
+  // async getAllEmployeeResults(iterator, isHistory) {
+  //   let allResults = [];
+  //   while (true) {
+  //     let res = await iterator.next();
+
+  //     if (res.value && res.value.value.toString()) {
+  //       let jsonRes = {};
+  //       console.log(res.value.value.toString("utf8"));
+
+  //       if (isHistory && isHistory === true) {
+  //         jsonRes.Timestamp = res.value.timestamp;
+  //       }
+  //       jsonRes.Key = res.value.key;
+
+  //       try {
+  //         jsonRes.Record = JSON.parse(res.value.value.toString("utf8"));
+  //       } catch (err) {
+  //         console.log(err);
+  //         jsonRes.Record = res.value.value.toString("utf8");
+  //       }
+  //       allResults.push(jsonRes);
+  //     }
+  //     if (res.done) {
+  //       console.log("end of data");
+  //       await iterator.close();
+  //       console.info(allResults);
+  //       return allResults;
+  //     }
+  //   }
+  // }
 }
 module.exports = PrimaryContract;
